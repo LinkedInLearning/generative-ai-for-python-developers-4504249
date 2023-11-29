d99eb36bd9d3ecfb6771ec526c986801c3e56284
Sandy Instructor <sandyludosky@gmail.com>
2023-11-29 19:01:19 +0100

    03_08e

--- exercise-files/3/03_08/handlers.py
+++ exercise-files/3/03_08/handlers.py
@@ -6,9 +6,9 @@ load_dotenv()
 client = openai.OpenAI()
 
 # Constants
-PERSONA = "A skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
+PERSONA = "You are a skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
 MODEL_ENGINE = "gpt-3.5-turbo"
-MESSAGE_SYSTEM = "A skilled stand-up comedian with a knack for telling funny stories."
+MESSAGE_SYSTEM = " You are a skilled stand-up comedian with a knack for telling 1-2 sentence funny stories."
 messages = [{"role": "system", "content": MESSAGE_SYSTEM}]
 
 
@@ -28,9 +28,12 @@ def print_messages(messages):
 
 
 def generate_chat_completion(user_input=""):
+    messages.append({"role": "user", "content": user_input})
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
     )
-
+    message = completion.choices[0].message
+    messages.append(to_dict(message))
     print(completion)
+    return message.content
--- exercise-files/3/03_08/main.py
+++ exercise-files/3/03_08/main.py
@@ -12,5 +12,6 @@ with st.form("user_form", clear_on_submit=True):
 # Press Enter to generate response from chatbot
 
 if submit_button:
-    pass
-    # generate chat completion
+    with st.spinner("Wait for it..."):
+        completion = generate_chat_completion(user_input)
+        st.write(completion)
