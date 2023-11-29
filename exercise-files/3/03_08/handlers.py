ec338db6131263f5eff8f81695be0fbf27009dad
Sandy Instructor <sandyludosky@gmail.com>
2023-11-29 19:02:20 +0100

    03_07e

--- exercise-files/3/03_08/handlers.py
+++ exercise-files/3/03_08/handlers.py
@@ -6,9 +6,9 @@ load_dotenv()
 client = openai.OpenAI()
 
 # Constants
-PERSONA = "You are a skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
+PERSONA = "A skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
 MODEL_ENGINE = "gpt-3.5-turbo"
-MESSAGE_SYSTEM = " You are a skilled stand-up comedian with a knack for telling 1-2 sentence funny stories."
+MESSAGE_SYSTEM = "A skilled stand-up comedian with a knack for telling funny stories."
 messages = [{"role": "system", "content": MESSAGE_SYSTEM}]
 
 
@@ -28,7 +28,6 @@ def print_messages(messages):
 
 
 def generate_chat_completion(user_input=""):
-    messages.append({"role": "user", "content": user_input})
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages,
