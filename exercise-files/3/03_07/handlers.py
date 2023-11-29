268160d8df45ae3589cb510e2d6f1fc0b6d12edd
Sandy Instructor <sandyludosky@gmail.com>
2023-11-29 19:03:19 +0100

    03_08b

--- exercise-files/3/03_07/handlers.py
+++ exercise-files/3/03_07/handlers.py
@@ -1,6 +1,4 @@
 import openai
-import os
-import json
 from dotenv import load_dotenv
 from colorama import Fore
 
@@ -8,27 +6,26 @@ load_dotenv()
 client = openai.OpenAI()
 
 # Constants
-PERSONA = "A skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
+PERSONA = "You are a skilled stand-up comedian with a quick wit and charismatic presence, known for their clever storytelling and ability to connect with diverse audiences through humor that is both insightful and relatable."
 MODEL_ENGINE = "gpt-3.5-turbo"
-MESSAGE_SYSTEM = "A skilled stand-up comedian with a knack for telling funny stories."
+MESSAGE_SYSTEM = " You are a skilled stand-up comedian with a knack for telling 1-2 sentence funny stories."
 messages = [{"role": "system", "content": MESSAGE_SYSTEM}]
 
 
+def to_dict(obj):
+    return {
+        "content": obj.content,
+        "role": obj.role,
+    }
+
+
 def print_messages(messages):
-    messages = [
-        json.dumps(message) for message in messages if message["role"] != "system"
-    ]
+    messages = [message for message in messages if message["role"] != "system"]
     for message in messages:
-        m = json.loads(message)
-        role = "Bot" if m["role"] == "assistant" else "You"
-        print(Fore.BLUE + role + ": " + m["content"])
+        role = "Bot" if message["role"] == "assistant" else "You"
+        print(Fore.BLUE + role + ": " + message["role"])
     return messages
 
 
 def generate_chat_completion(user_input=""):
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-    )
-
-    print(completion)
+    pass
