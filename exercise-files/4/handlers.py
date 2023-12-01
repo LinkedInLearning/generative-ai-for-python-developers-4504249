8774a222066d37cffebad5d27608b3485d47a795
Sandy Instructor <sandyludosky@gmail.com>
2023-12-01 12:46:26 +0100

    04_02e

--- exercise-files/4/handlers.py
+++ exercise-files/4/handlers.py
@@ -13,10 +13,15 @@ messages = [{"role": "system", "content": MESSAGE_SYSTEM}]
 
 
 def moderate(user_input):
-    pass
+    response = client.moderations.create(input=user_input)
+    return response.results[0].flagged
 
 
 def generate_chat_completion(user_input, messages):
+    flagged = moderate(user_input)
+    print(f"Flagged: {flagged}")
+    if flagged:
+        return ":red[Your comment has been flagged as inappropriate.]"
     completion = client.chat.completions.create(
         model=MODEL_ENGINE,
         messages=messages,
